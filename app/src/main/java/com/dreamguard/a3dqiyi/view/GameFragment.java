79499f37c5c0266b5afa30ac14a96c9d2616eb16
hailin <hailin_dai@163.com>
2018-05-11 18:08:29 +0800

    初步增加游戏界面banner

--- app/src/main/java/com/dreamguard/a3dqiyi/view/GameFragment.java
+++ app/src/main/java/com/dreamguard/a3dqiyi/view/GameFragment.java
@@ -1,19 +1,36 @@
 package com.dreamguard.a3dqiyi.view;
 
 
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
 import com.dreamguard.a3dqiyi.R;
+import com.dreamguard.a3dqiyi.utils.GlideCircleTransform;
+import com.dreamguard.a3dqiyi.utils.GlideRoundTransform;
+import com.dreamguard.a3dqiyi.view.video.GameBannerAdapter;
 
 /**
  * Created by hailin.dai on 2018/5/6.
  * Email: 605345320@qq.com
  */
 public class GameFragment extends Fragment {
+
+
+    private ViewPager gameBannerViewPager;
+    private PagerAdapter gameBannerAdapter;
+
 
 
     public GameFragment() {
@@ -25,7 +42,75 @@ public class GameFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_game, container, false);
+        View view = inflater.inflate(R.layout.fragment_game, container, false);
+        gameBannerViewPager = (ViewPager) view.findViewById(R.id.game_banner);
+        return view;
     }
 
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        gameBannerAdapter = new GameBannerAdapter(getActivity());
+        gameBannerViewPager.setPageMargin(10);
+        gameBannerViewPager.setAdapter(gameBannerAdapter);
+
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        Log.d("dai","onStart");
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        Log.d("dai","onResume");
+        startScrollBanner();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        Log.d("dai","onPause");
+        stopScrollBanner();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        Log.d("dai","onStop");
+    }
+
+    private boolean isLooper;
+
+    public void startScrollBanner(){
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                isLooper = true;
+                while (isLooper){
+                    try {
+                        Thread.sleep(2000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+
+                    getActivity().runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            //这里是设置当前页的下一页
+                            gameBannerViewPager.setCurrentItem(gameBannerViewPager.getCurrentItem() + 1);
+                        }
+                    });
+                }
+            }
+        }).start();
+    }
+
+    public void stopScrollBanner(){
+        isLooper = false;
+    }
 }
---
+++ app/src/main/java/com/dreamguard/a3dqiyi/view/video/GameBannerAdapter.java
@@ -0,0 +1,117 @@
+package com.dreamguard.a3dqiyi.view.video;
+
+import android.content.Context;
+import android.net.Uri;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.dreamguard.a3dqiyi.R;
+import com.dreamguard.a3dqiyi.utils.GlideRoundTransform;
+
+/**
+ * <pre>
+ *     author : daihailin
+ *     e-mail : daihl@kdxfilm.com
+ *     time   : 2018/05/11
+ *     desc   :
+ *     version: 1.0
+ * </pre>
+ */
+
+public class GameBannerAdapter extends PagerAdapter{
+
+    private Context mContext;
+    public GameBannerAdapter(Context context) {
+        mContext = context;
+    }
+
+    private String[] imgRes = {
+            "http://pic3.qiyipic.com/common/lego/20180510/a090c0b50deb42c8952a35ebf8895ec2.jpg",
+            "http://pic34.nipic.com/20131014/9483785_151836168001_2.jpg",
+            "http://pic.58pic.com/58pic/13/17/92/22P58PICYyR_1024.jpg",
+            "http://scimg.jb51.net/allimg/150430/14-1504300U01J49.jpg",
+            "http://pic51.nipic.com/file/20141031/8098773_215235317000_2.jpg"
+    };
+
+    @Override
+    public Object instantiateItem(ViewGroup container, int position) {
+        View view = LayoutInflater.from(mContext).inflate(R.layout.fragment_game_banner_item, null);
+        ImageView image = (ImageView) view.findViewById(R.id.game_banner_image);
+        image.setScaleType(ImageView.ScaleType.FIT_XY);
+        final int realPosition = getRealPosition(position);
+        image.setImageURI(Uri.parse(imgRes[realPosition]));
+        Glide.with(mContext)
+                .load(imgRes[realPosition])
+                .transform( new GlideRoundTransform(mContext))
+                .diskCacheStrategy(DiskCacheStrategy.ALL)
+                .crossFade()
+                .into(image);
+        container.addView(view);
+
+        view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            }
+        });
+        return view;
+    }
+
+
+    @Override
+    public int getItemPosition(Object object) {
+        return POSITION_NONE;
+    }
+
+    @Override
+    public void destroyItem(ViewGroup container, int position, Object object) {
+        container.removeView((View) object);
+    }
+
+    @Override
+    public int getCount() {
+        return Integer.MAX_VALUE;
+    }
+
+    @Override
+    public boolean isViewFromObject(View view, Object o) {
+        return view == o;
+    }
+
+    @Override
+    public void startUpdate(ViewGroup container) {
+        super.startUpdate(container);
+        ViewPager viewPager = (ViewPager) container;
+        int position = viewPager.getCurrentItem();
+        if (position == 0) {
+            position = getFirstItemPosition();
+        } else if (position == getCount() - 1) {
+            position = getLastItemPosition();
+        }
+        viewPager.setCurrentItem(position, false);
+
+    }
+
+    private int getRealCount() {
+        return imgRes.length;
+    }
+
+    private int getRealPosition(int position) {
+        return position % getRealCount();
+    }
+
+    private int getFirstItemPosition() {
+        return Integer.MAX_VALUE / 2;
+    }
+
+    private int getLastItemPosition() {
+        return Integer.MAX_VALUE / 2 - 1;
+    }
+
+}
